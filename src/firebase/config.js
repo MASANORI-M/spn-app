4b46784f45d6ea0633085abf97fd54973e5018f0
MASANORI-M <kougakumasa.1031@gmail.com>
2021-12-29 16:19:24 +0900

    first commit

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,10 +1,4 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
-    apiKey: "AIzaSyD2fsAjQOXdchheqq2Dwj_Q6d_GakhjPYs",
-    authDomain: "spn-app-fed9b.firebaseapp.com",
-    projectId: "spn-app-fed9b",
-    storageBucket: "spn-app-fed9b.appspot.com",
-    messagingSenderId: "304215223935",
-    appId: "1:304215223935:web:c903858360d35f07a6594a",
-    measurementId: "G-4ET2R3QKY0"
+    
 };
